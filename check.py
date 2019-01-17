62804fae472fbe39d904530c1fde517d1fca2c46
Laurence Tratt <laurie@tratt.net>
2019-01-17 08:16:16 +0000

    Error if required files are missing.

--- check.py
+++ check.py
@@ -12,17 +12,23 @@ for tl in os.listdir(TEAMS_DIR):
         continue
     if not os.path.isdir(tl):
         continue
-    mems = [x.strip() for x in open(os.path.join(TEAMS_DIR, tl, "members.txt")).readlines()]
-    for m in mems:
-        if m == "":
-            continue
-        if m in allocd:
-            print("Error: %s appears in multiple teams" % m)
+    for l in ["coordinator.txt", "members.txt", "repo.txt"]:
+        if not os.path.exists(os.path.join(TEAMS_DIR, tl, l)):
+            print("Error: File %s is missing" % l)
             err = True
-        if m not in alls:
-            print("Error: %s not in student records" % m)
-            err = True
-        allocd.add(m)
+    mp = os.path.join(TEAMS_DIR, tl, "members.txt")
+    if os.path.exists(mp):
+        mems = [x.strip() for x in open(mp).readlines()]
+        for m in mems:
+            if m == "":
+                continue
+            if m in allocd:
+                print("Error: %s appears in multiple teams" % m)
+                err = True
+            if m not in alls:
+                print("Error: %s not in student records" % m)
+                err = True
+            allocd.add(m)
 
 if err:
     sys.exit(1)
